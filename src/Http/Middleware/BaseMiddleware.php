16af92c511079924012b3a542dbe948baf5aebea
jybtx <2574034626@qq.com>
2021-02-05 12:36:54 +0800

    修改中间键

--- src/Http/Middleware/BaseMiddleware.php
+++ src/Http/Middleware/BaseMiddleware.php
@@ -2,32 +2,16 @@
 
 namespace Jybtx\TokenAuth\Http\Middleware;
 
-use Illuminate\Http\Request;
 use Jybtx\TokenAuth\JwtAuthToken;
 use Illuminate\Support\Facades\Cache;
 use Jybtx\TokenAuth\Support\CreateToken;
 use Jybtx\TokenAuth\Support\TokenValidator;
 use Jybtx\TokenAuth\Support\TokenBlackList;
 use Jybtx\TokenAuth\Support\AuthenticationHeader;
-use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 abstract class BaseMiddleware
 {
-	use TokenValidator,AuthenticationHeader
-        ,TokenBlackList,CreateToken;
-    /**
-     * [Check the token value of the user's web page]
-     * @author jybtx
-     * @date   2019-12-16
-     * @return [type]     [description]
-     */
-	public function checkForToken()
-	{
-		if ( !$this->getVerifyToken( getoken() ) ) 
-        {
-			throw new UnauthorizedHttpException('token-auth', 'Token not provided');
-		}
-	}
+	use TokenValidator,AuthenticationHeader,TokenBlackList,CreateToken;
     /**
      * [checkTokenRefreshTimeForRestApi description]
      * @author jybtx
@@ -58,20 +42,9 @@ abstract class BaseMiddleware
      */
     public function setAuthenticationHeaders($response, $token = null)
     {
-        return $this->setAuthenticationHeader($response, $token);
+        return $this->getSetAuthenticationHeader($response, $token);
     }
 
-    /**
-     * Check whether token is in blacklist
-     *
-     */
-    public function checkTokenIsInBlacklist()
-    {
-        if ( Cache::has( getoken() ) )
-        {
-            throw new UnauthorizedHttpException('token-auth', 'Token expired');
-        }
-    }
     /**
      * [Check the blacklist token value of the user REST API]
      * @author jybtx
